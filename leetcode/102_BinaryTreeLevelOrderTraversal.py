2a376f7213c8e536a7508cdd5bdc98df8ec0f579
NafisAslam70 <nafisaslam70@gmail.com>
2025-12-30 16:55:08 +0530

    ✅ Daily update: 2025-12-30

--- leetcode/102_BinaryTreeLevelOrderTraversal.py
+++ leetcode/102_BinaryTreeLevelOrderTraversal.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution(object):
     def levelOrder(self, root):
         """
